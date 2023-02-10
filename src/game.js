b958bbfe749f82ea4fde5c65c7109ebe910c0ecb
Glauber Apolinario <glauber.apolinario@yahoo.com.br>
2023-02-10 10:00:43 -0300

    adição do player

--- src/game.js
+++ src/game.js
@@ -22,11 +22,13 @@ function init ()
 function preload ()
 {
     this.load.image('spr_bg', "src/assets/img/spr_bg.png")
+    this.load.image('spr_player', 'src/assets/img/spr_player.png')
 }
 
 function create ()
 {
     this.add.image(0, 0, 'spr_bg').setOrigin(0, 0)
+    obj_player = this.add.image(400, 440, 'spr_player').setOrigin(0.5, 1);
 }
 
 function update ()
